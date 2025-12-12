77445018a35b5ed351534f122233177fe6480134
OSOMAGIC <winosoapp@gmail.com>
2025-12-12 01:32:51 +0100

    Crear constants/Colors.ts

---
+++ constants/Colors.ts
@@ -0,0 +1,18 @@
+const Colors = {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: '#007AFF',
+    tabIconDefault: '#ccc',
+    tabIconSelected: '#007AFF',
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: '#007AFF',
+    tabIconDefault: '#ccc',
+    tabIconSelected: '#007AFF',
+  },
+};
+
+export default Colors;
