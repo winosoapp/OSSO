fec11e96bf672aa5a512a1fb41687633b7c005fc
OSOMAGIC <winosoapp@gmail.com>
2025-12-19 18:45:10 +0100

    Crear src/utils/index.ts

---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+/**
+ * OSSO - Utilidades Centralizadas
+ * Exporta todas las utilidades de la aplicación
+ */
+
+export * from './logger';
+export * from './validators';
+export * from './formatters';
