81c9958f2edb511361ff507641a446f00a366eb2
OSOMAGIC <winosoapp@gmail.com>
2025-12-19 13:55:21 +0100

    Crear src/utils/constants.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,15 @@
+// Constantes de la aplicación
+export const APP_NAME = 'OSSO';
+export const APP_VERSION = '1.0.0';
+
+// Colores
+export const COLORS = {
+  primary: '#007AFF',
+  secondary: '#5856D6',
+  success: '#34C759',
+  warning: '#FF9500',
+  danger: '#FF3B30',
+  background: '#FFFFFF',
+  text: '#000000',
+  textSecondary: '#666666',
+};
