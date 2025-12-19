692d746435d85587b0d76b6b77ccd8e0ab68f017
OSOMAGIC <winosoapp@gmail.com>
2025-12-19 18:44:31 +0100

    Crear src/constants/index.ts

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+/**
+ * OSSO - Constantes Centralizadas
+ * Exporta todas las constantes de la aplicación
+ */
+
+export { Colors } from './colors';
+export { Spacing, BorderRadius, FontSize, FontWeight } from './spacing';
+
+// Re-exportar constantes de configuración
+export { APP_CONSTANTS, TIMEOUTS, ENDPOINTS } from '../config';
