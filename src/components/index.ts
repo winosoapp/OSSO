24a9517d8eb97f4d61feef5ccca7915c734f7d77
OSOMAGIC <winosoapp@gmail.com>
2025-12-19 18:49:04 +0100

    Crear src/components/index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+/**
+ * OSSO - Componentes Centralizados
+ * Exporta todos los componentes reutilizables
+ */
+
+export { Button } from './Button';
+export { Card } from './Card';
+export { Input } from './Input';
+export { Loading } from './Loading';
