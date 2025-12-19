5e42bdf4c6d605b405ae3d3f71ca317b0e3d7c17
OSOMAGIC <winosoapp@gmail.com>
2025-12-19 18:44:17 +0100

    Crear src/constants/colors.ts

---
+++ src/constants/colors.ts
@@ -0,0 +1,51 @@
+/**
+ * OSSO - Paleta de Colores
+ * Sistema de diseño consistente
+ */
+
+export const Colors = {
+  // Colores principales
+  primary: '#007AFF',
+  primaryDark: '#0051D5',
+  primaryLight: '#5AC8FA',
+  
+  // Colores secundarios
+  secondary: '#5856D6',
+  secondaryDark: '#3634A3',
+  secondaryLight: '#8E8CD8',
+  
+  // Estados
+  success: '#34C759',
+  warning: '#FF9500',
+  error: '#FF3B30',
+  info: '#007AFF',
+  
+  // Grises
+  gray1: '#F2F2F7',
+  gray2: '#E5E5EA',
+  gray3: '#D1D1D6',
+  gray4: '#C7C7CC',
+  gray5: '#AEAEB2',
+  gray6: '#8E8E93',
+  
+  // Texto
+  text: '#000000',
+  textSecondary: '#3C3C43',
+  textTertiary: '#3C3C4399',
+  textDisabled: '#3C3C434D',
+  
+  // Fondo
+  background: '#FFFFFF',
+  backgroundSecondary: '#F2F2F7',
+  backgroundTertiary: '#FFFFFF',
+  
+  // Bordes
+  border: '#C6C6C8',
+  borderLight: '#E5E5EA',
+  
+  // Transparencias
+  overlay: 'rgba(0, 0, 0, 0.4)',
+  overlayLight: 'rgba(0, 0, 0, 0.1)',
+};
+
+export default Colors;
